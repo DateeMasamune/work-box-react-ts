3b4f2f5485196b49419eee74cded4b4f8fcd2eaf
Dmitry Ovsienko <ovsienkodima@mail.ru>
2023-05-07 21:54:32 +0300

    fix name

--- src/web-worker.ts
+++ src/web-worker.ts
@@ -1,7 +1,7 @@
 self.addEventListener('message', ({ data }) => {
   // eslint-disable-next-line prefer-template, no-new-func
-  const cumputed = new Function('return' + data)();
-  cumputed();
+  const computed = new Function('return' + data)();
+  computed();
   postMessage('result');
 });
 
